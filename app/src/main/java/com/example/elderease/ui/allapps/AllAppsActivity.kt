062b2ae1dd98845149aa0ef0a99572d169f96027
Nileena <nileenas123@gmail.com>
2026-02-17 16:38:35 +0530

    Added Home UI + Settings + Emergency screens

--- app/src/main/java/com/example/elderease/ui/allapps/AllAppsActivity.kt
+++ app/src/main/java/com/example/elderease/ui/allapps/AllAppsActivity.kt
@@ -2,6 +2,7 @@ package com.example.elderease.ui.allapps
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
@@ -16,6 +17,9 @@ class AllAppsActivity : AppCompatActivity() {
         setContentView(R.layout.activity_all_apps)
 
         val recyclerView = findViewById<RecyclerView>(R.id.recyclerAllApps)
+        val btnBackHome = findViewById<Button>(R.id.btnBackHome)
+        val btnHomeBottom = findViewById<Button>(R.id.btnHomeBottom)
+
         recyclerView.layoutManager = GridLayoutManager(this, 3)
 
         val apps = loadAllApps()
@@ -23,6 +27,16 @@ class AllAppsActivity : AppCompatActivity() {
         recyclerView.adapter = AppAdapter(apps) { app ->
             app.launchIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(app.launchIntent)
+        }
+
+        // Top right Home button
+        btnBackHome.setOnClickListener {
+            finish()
+        }
+
+        // Bottom Home button
+        btnHomeBottom.setOnClickListener {
+            finish()
         }
     }
 
@@ -33,12 +47,15 @@ class AllAppsActivity : AppCompatActivity() {
 
         val resolveInfos = pm.queryIntentActivities(intent, 0)
 
-        val apps = resolveInfos.map {
+        val apps = resolveInfos.mapNotNull {
+
+            val launchIntent = pm.getLaunchIntentForPackage(it.activityInfo.packageName)
+                ?: return@mapNotNull null   // prevents crash
+
             val label = it.loadLabel(pm).toString()
             val icon = it.loadIcon(pm)
-            val launchIntent = pm.getLaunchIntentForPackage(it.activityInfo.packageName)
 
-            AppInfo(label, icon, launchIntent!!)
+            AppInfo(label, icon, launchIntent)
         }
 
         return apps.sortedBy { it.label.lowercase() }
